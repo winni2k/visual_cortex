cd8e7938e55731cd2d3b62cfe093670b97415ada
Warren W. Kretzschmar <wkretzsch@gmail.com>
2017-09-16 17:35:15 +0200

    Adds link to links

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from io import StringIO
 
 import attr
+from attr import Factory
 from flask import Flask, render_template, redirect
 from flask_bootstrap import Bootstrap
 from subprocess import check_output
@@ -30,11 +31,21 @@ class Kmer(object):
     canonical_kmer = attr.ib(init=False)
     oriented_kmer = attr.ib(init=False)
     coverage = attr.ib(init=False)
-    edge_split = attr.ib(init=False)
+    _edge_split = attr.ib(init=False)
+    forward_links = attr.ib(default=Factory(dict))
+    backward_links = attr.ib(default=Factory(dict))
 
     def __attrs_post_init__(self):
         fields = self.line.strip().split()
-        self.canonical_kmer, self.oriented_kmer, self.coverage, self.edge_split = tuple(fields[:4])
+        self.canonical_kmer, self.oriented_kmer, self.coverage, self._edge_split = tuple(fields[:4])
+        edges = list(self._edge_split)
+        assert (len(edges) == 8)
+        for index, edge in enumerate(edges):
+            if edge != '.':
+                if index < 4:
+                    self.backward_links[edge] = edge + self.oriented_kmer
+                else:
+                    self.forward_links[edge] = self.oriented_kmer + edge
 
 
 class MyForm(FlaskForm):
@@ -61,8 +72,9 @@ def parse_cortexjdk_print(stream):
 
 @app.route('/kmers/<kmer>')
 def kmers_show(kmer):
+    kmer = kmer.upper()
     output = StringIO(
         check_output(['java', '-jar', CORTEXJDK_JAR, 'Print', '--graph', GRAPH, '--record',
                       kmer]).decode())
     kmers = parse_cortexjdk_print(output)
-    return '\n'.join([str(k) for k in kmers])
+    return render_template('kmers.html', kmers=kmers)
