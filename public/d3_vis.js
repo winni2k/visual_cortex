0f34714a34c8c41f3df111ce920b694d56584085
Warren W. Kretzschmar <wkretzsch@gmail.com>
2017-10-26 17:12:29 +0200

    working on graph: click reveals step_graph

--- public/d3_vis.js
+++ public/d3_vis.js
@@ -4,10 +4,10 @@
 var node_height = 60
 
 d3.json("graph.json", function (graph) {
-    let svg = d3.select('svg');
+    let svg = d3.select('svg')
 
     let nodes = {}
-    for (let n of graph.nodes){
+    for (let n of graph.nodes) {
         nodes[n.id] = n
     }
     let node_ids = graph.nodes.map(n => n.id)
@@ -19,19 +19,41 @@ d3.json("graph.json", function (graph) {
         .append("g")
         .attr('class', 'node')
         .merge(node) // update entering and existing elements
-        .each(bind_node_vis)
+        .each(build_node)
 
+    function node_width(node_data) {
+        return node_data.repr.length * 8
+    }
 
-    function bind_node_vis(node_id, i) {
-        node_data = nodes[node_id]
-        //d = d['data']
-        let id = node_id
-        // console.log(node_data)
-        d3.select(this)
-            .attr('id', id)
+    function build_node(node_id, i) {
+        let node_data = nodes[node_id]
+
+        let node = d3.select(this)
+            .attr('id', node_id)
             .attr('transform', `translate(100,${i * (node_height + 10) })`)
 
-        let width = node_data.repr.length * 8
+        let stroke_width = 2
+        node
+            .append('text')
+            .text(node_data.repr)
+            .attrs({y: 20, x: 5})
+        node
+            .append('rect')
+            .attrs({
+                width: node_width(node_data),
+                height: node_height,
+                'fill-opacity': 0,
+                stroke: 'blue',
+                'stroke-width': stroke_width,
+            })
+            .on('click', bind_node_vis)
+    }
+
+    function bind_node_vis(node_id, i) {
+        console.log(`click! ${node_id} ${i}`)
+        let node_data = nodes[node_id]
+
+        let width = node_width(node_data)
         let chart = bb.generate({
             data: {
                 type: "step",
@@ -51,10 +73,10 @@ d3.json("graph.json", function (graph) {
                 y: {show: false},
             },
             legend: {show: false},
-            bindto: this,
+            bindto: this.parentNode,
         }).resize({width: width, height: node_height})
     }
-});
+})
 
 //
 
