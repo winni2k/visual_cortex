820f8f995c2f02818e3f5763908531b518a220c3
Warren W. Kretzschmar <wkretzsch@gmail.com>
2017-10-26 16:15:05 +0200

    working on graph2

--- public/d3_vis.js
+++ public/d3_vis.js
@@ -1,58 +1,59 @@
 /**
  * Created by winni on 10/25/17.
  */
+var node_height = 60
 
 d3.json("graph.json", function (graph) {
-    var svg = d3.select('svg');
+    let svg = d3.select('svg');
 
-    var rect_width = 300;
-    var rect_height = 30;
-    var node = svg.selectAll('.node')
-        .data([graph.nodes[0]])
-        .style('fill', 'blue');
+    let nodes = {}
+    for (let n of graph.nodes){
+        nodes[n.id] = n
+    }
+    let node_ids = graph.nodes.map(n => n.id)
 
-    node.exit().remove();
+    let node = svg.selectAll('.node')
+        .data(node_ids)
 
-    node = node.enter()
+    node.enter()
         .append("g")
-        .attr('id', 0)
         .attr('class', 'node')
-        .attr('transform', 'translate(100,100)');
-
-    // node.append('rect')
-    //     .style("fill", "green")
-    //     .merge(node)
-    //     .style("stroke", "black")
-    //     .attr('width', rect_width)
-    //     .attr('height', rect_height)
-    //     .attr('opacity', 0.3)
-    // node.append('g').attr('id', 0)
+        .merge(node) // update entering and existing elements
+        .each(bind_node_vis)
 
 
-    var chart = bb.generate({
-        bindto: document.getElementById('0'),
-        data: {
-            type: "step",
-            columns: [
-                ["1", 30, 200, 100, 170],
-                ["2", 130, 100, 140, 35]
-            ],
-        },
-        axis: {
-            x: {
-                type: 'category',
-                tick: {
-                    outer: false,
-                    centered: true
-                },
-                categories: ["A", "C", "G", "T"],
+    function bind_node_vis(node_id, i) {
+        node_data = nodes[node_id]
+        //d = d['data']
+        let id = node_id
+        // console.log(node_data)
+        d3.select(this)
+            .attr('id', id)
+            .attr('transform', `translate(100,${i * (node_height + 10) })`)
+
+        let width = node_data.repr.length * 8
+        let chart = bb.generate({
+            data: {
+                type: "step",
+                columns: [
+                    ['Coverage'].concat(node_data.coverage)
+                ],
             },
-            y: {show: false},
-        },
-        legend: {show: false}
-
-    }).resize({width: 300, height: 100});
-
+            axis: {
+                x: {
+                    type: 'category',
+                    tick: {
+                        outer: false,
+                        centered: true
+                    },
+                    categories: node_data.repr.split(''),
+                },
+                y: {show: false},
+            },
+            legend: {show: false},
+            bindto: this,
+        }).resize({width: width, height: node_height})
+    }
 });
 
 //
