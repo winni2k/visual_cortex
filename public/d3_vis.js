0b64f0bedc09d3d670810072e11fe70e8cf9e7cd
Warren W. Kretzschmar <wkretzsch@gmail.com>
2017-10-29 15:25:48 +0100

    Getting webcola to work

--- public/d3_vis.js
+++ public/d3_vis.js
@@ -18,37 +18,118 @@ var svgViewport = d3.select("body")
 //.style('stroke', 'black')
 
 
-svgViewport.append("rect")
-    .attr("width", svgWidth)
-    .attr("height", svgHeight)
-    .style("fill", "none")
-    .style("pointer-events", "all")
-    .call(d3.zoom()
-        .scaleExtent([1 / 8, 4])
-        .on("zoom", zoomed))
+// svgViewport.append("rect")
+//     .attr("width", svgWidth)
+//     .attr("height", svgHeight)
+//     .style("fill", "none")
+//     .style("pointer-events", "all")
+//     .call(d3.zoom()
+//         .scaleExtent([1 / 8, 4])
+//         .on("zoom", zoomed))
 
-var svg = svgViewport.append('g')
+// var svg = svgViewport.append('g')
+var svg = svgViewport
+
+var color = d3.scaleOrdinal(d3.schemeCategory20);
 
 
-var simulation = d3.forceSimulation()
-    .force("link", d3.forceLink().id(d => d.id))
-    .force('repel_end_nodes', d3.forceLink().id(d => d.id))
-    //.force("charge", d3.forceManyBody().strength(-0.1))
-    .force("center", d3.forceCenter(width / 2, height / 2))
-    .force('collision', d3.forceCollide().radius(n => Math.sqrt(get_node_height(n) ** 2 + get_node_width(n) ** 2) / 2))
-    .force('vertical', d3.forceX(svgWidth))
+var d3cola = cola.d3adaptor(d3)
+    .avoidOverlaps(true)
+    .size([width, height])
+
+
+// var simulation = d3.forceSimulation()
+//     .force("link", d3.forceLink().id(d => d.id))
+//     .force('repel_end_nodes', d3.forceLink().id(d => d.id))
+//     //.force("charge", d3.forceManyBody().strength(-0.1))
+//     .force("center", d3.forceCenter(width / 2, height / 2))
+//     .force('collision', d3.forceCollide().radius(get_node_radius())
+//     .force('vertical', d3.forceX(svgWidth))
 //.force('circular', d3.forceRadial(height * 3, width / 2, height / 2).strength(0.3))
 
-d3.json("graph.json", graph => {
+d3.json('graph.json', graph => {
+    var nodeRadius = 5
 
-    var min_node_id = graph.nodes.reduce((prev, curr) => {
-        return prev.id < curr.id ? prev : curr
+    graph.nodes.forEach(v => {
+        v.height = v.width = 2 * nodeRadius
     })
-    var max_node_id = graph.nodes.reduce((prev, curr) => {
-        return prev.id < curr.id ? curr : prev
-    })
-    var total_node_radius = graph.nodes.reduce((sum, node) => sum + get_node_width(node), 0)
 
+    console.log(graph.nodes)
+    d3cola
+        .nodes(graph.nodes)
+        .links(graph.edges)
+        .flowLayout("y", 30)
+        .symmetricDiffLinkLengths(6)
+        .start(10, 20, 20)
+
+    // define arrow markers for graph links
+    svg.append('svg:defs').append('svg:marker')
+        .attr('id', 'end-arrow')
+        .attr('viewBox', '0 -5 10 10')
+        .attr('refX', 6)
+        .attr('markerWidth', 3)
+        .attr('markerHeight', 3)
+        .attr('orient', 'auto')
+        .append('svg:path')
+        .attr('d', 'M0,-5L10,0L0,5')
+        .attr('fill', '#000')
+
+    var path = svg.selectAll(".link")
+        .data(graph.links)
+        .enter().append('svg:path')
+        .attr('class', 'link')
+
+    var node = svg.selectAll(".node")
+        .data(graph.nodes)
+        .enter().append("circle")
+        .attr("class", "node")
+        .attr("r", nodeRadius)
+        .style("fill", function (d) {
+            return color(d.group)
+        })
+        .call(d3cola.drag)
+
+    node.append("title")
+        .text(function (d) {
+            return d.name
+        })
+
+    d3cola.on("tick", function () {
+        // draw directed edges with proper padding from node centers
+        path.attr('d', function (d) {
+            var deltaX = d.target.x - d.source.x,
+                deltaY = d.target.y - d.source.y,
+                dist = Math.sqrt(deltaX * deltaX + deltaY * deltaY),
+                normX = deltaX / dist,
+                normY = deltaY / dist,
+                sourcePadding = nodeRadius,
+                targetPadding = nodeRadius + 2,
+                sourceX = d.source.x + (sourcePadding * normX),
+                sourceY = d.source.y + (sourcePadding * normY),
+                targetX = d.target.x - (targetPadding * normX),
+                targetY = d.target.y - (targetPadding * normY)
+            return 'M' + sourceX + ',' + sourceY + 'L' + targetX + ',' + targetY
+        })
+
+        node.attr("cx", function (d) {
+            return d.x
+        })
+            .attr("cy", function (d) {
+                return d.y
+            })
+    })
+
+})
+
+function unused(graph) {
+    d3cola
+        .nodes(graph.nodes)
+        .links(graph.edges)
+        .linkDistance(100)
+        //.avoidOverlaps(true)
+        .start(30)
+
+    console.log('past start')
     var link = svg.append("g")
         .attr("class", "links")
         .selectAll("line")
@@ -68,31 +149,22 @@ d3.json("graph.json", graph => {
         nodes[n.id] = n
     }
 
-    var node_container = svg.append('g')
+    var node = svg.append('g')
         .attr('class', 'nodes')
         .selectAll('.node')
         .data(graph.nodes)
         .enter()
         .append('g')
         .attrs({
-            id: n => `${n.id}.container`,
+            id: n => n.id,
             width: n => get_node_width(n) + 2 * node_padding,
             height: get_node_height,
-        })
-        .call(d3.drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended))
-
-    var node = node_container
-        .append('g')
-        .attrs({
-            id: n => n.id,
-            transform: n => `translate(${get_node_width(n) / 2},${get_node_height(n) / 2})`
+            'class': 'node'
         })
 
 
-    node_container
+    //node.call(d3cola.drag)
+    node
         .append('rect')
         .attrs({
             width: d => get_node_width(d) + 2 * node_padding,
@@ -104,7 +176,7 @@ d3.json("graph.json", graph => {
         })
         .on('click', bind_node_vis)
 
-    node_container
+    node
         .append('text')
         .text(n => n.repr)
         .attrs({
@@ -113,22 +185,17 @@ d3.json("graph.json", graph => {
             textLength: n => get_node_width(n),
         })
 
-    simulation
-        .nodes(graph.nodes)
-        .on("tick", tock)
 
+    // simulation.force('repel_end_nodes')
+    //     .links([{
+    //         source: min_node_id,
+    //         target: max_node_id,
+    //     }])
+    //     .distance(total_node_radius/2)
 
-    simulation.force("link")
-        .links(graph.edges)
-
-    simulation.force('repel_end_nodes')
-        .links([{
-            source: min_node_id,
-            target: max_node_id,
-        }])
-        .distance(total_node_radius/2)
-
+    d3cola.on('tick', tock)
     function tock() {
+        console.log('tock')
         link.attrs({
             x1: d => d.source.x,
             y1: d => d.source.y,
@@ -137,7 +204,6 @@ d3.json("graph.json", graph => {
         })
 
         node.attr("transform", n => `translate(${n.x},${n.y})`)
-        node_container.attr("transform", nc => `translate(${nc.x - get_node_width(nc) / 2},${nc.y - get_node_height(nc) / 2})`)
     }
 
     function bind_node_vis(node_id) {
@@ -168,7 +234,7 @@ d3.json("graph.json", graph => {
             bindto: this.parentNode,
         }).resize({width: width, height: node_height})
     }
-})
+}
 
 function dragstarted(d) {
     console.log(d3.event.target)
@@ -202,6 +268,10 @@ function get_node_width(node_data) {
     return Math.max(12, node_data.repr.length * 8)
 }
 
-function get_node_color(node_data){
+function get_node_color(node_data) {
     return node_data.is_missing ? 'grey' : 'blue'
+}
+
+function get_node_radius(node_data) {
+    return Math.sqrt(get_node_height(node_data) ** 2 + get_node_width(node_data) ** 2) / 2
 }
